bdff3bd8dae3f0b0ad2320dba0bc61993b7454f8
siddharthyadava <siddharthyadav2904@gmail.com>
2025-09-19 12:56:31 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useMemo, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
+
+  function expensiveTask(num) {
+    console.log("Inside Expensive Task");
+    for(let i=0; i<=1000000000; i++) {}
+    return num*2;
+  }
+
+  let doubleValue = useMemo(() => expensiveTask(input), [input]);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count+1)}>
+        Increment
+      </button>
+
+      <div>
+        Count: {count}
+      </div>
+      <br />
+
+      <input 
+      type="number"
+      placeholder='enter number'
+      value={input}
+      onChange={(e) => setInput(e.target.value)} />
+
+      <div>
+        Double: {doubleValue}
+      </div>
+    </div>
+  )
+}
+
+export default App
